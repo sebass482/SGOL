5c5127c731b81d27d8cbe4851afe439039f90fa6
Sebastian <sebastianmendoza482@gmail.com>
2021-05-19 23:18:24 -0700

    added useHistory and handleClick function

--- sebastians-game-of-life/src/components/LandingPage.tsx
+++ sebastians-game-of-life/src/components/LandingPage.tsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import { styled } from '@stitches/react';
 import CGOLGIF from '../assets/CGOL.gif';
-import {BrowserRouter as Router,Link} from 'react-router-dom';
+import { BrowserRouter as Router, Link, useHistory } from 'react-router-dom';
 
 
-
-function LandingPage(){
-    const StyledImg = styled('img',{
-        width:'100%',
+function LandingPage() {
+    const history = useHistory()
+    const StyledImg = styled('img', {
+        width: '100%',
         height: '100vh',
-        objectFit:'cover',
+        objectFit: 'cover',
     })
 
-    const StyledText=  styled('div',{
+    const StyledText = styled('div', {
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%);',
-        color:'white',
-        fontSize:'1rem'
+        color: 'white',
+        fontSize: '1rem'
     })
-    
-    return(
-    <>
-        <StyledImg src={CGOLGIF}/>
-        <StyledText> 
-        Welcome to Sebastian's Game of Life!
-        <br></br>
-        <Router>
-        <button>
-          <Link to="/rules">Rules</Link>
-          </button>
-          </Router>
-        </StyledText>
-    </>
+
+    const pushToGame = (event: React.MouseEvent) => {
+        event?.preventDefault();
+        history.push('/game')
+    }
+
+    return (
+        <>
+            <StyledImg src={CGOLGIF} />
+            <StyledText>
+                Welcome to Sebastian's Game of Life!
+                <br></br>
+                <button onClick={pushToGame}> Play!</button>
+            </StyledText>
+        </>
     )
 }
 export default LandingPage
